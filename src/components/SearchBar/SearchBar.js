ed29e679f38966ced9b50c185e903a90d9ea264f
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-15 15:13:45 +0900

    SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -10,16 +10,37 @@ function SearchBar() {
 		setSearch(event.target.value);
 	};
 
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
 	return (
 		<>
-			<form>
+			<form className={styles.searchBar} onSubmit={onSubmit}>
 				<input
 					placeholder="카테고리 검색하기"
 					value={search}
 					onChange={onChange}
+					className={styles.searchInput}
 				/>
-				<input type="submit" />
+				<button type="submit" className={styles.submitBtn}>
+					<img
+						src="/Magnifier.svg"
+						alt="돋보기"
+						className={styles.magnifier}
+					/>
+				</button>
 			</form>
+
+			<div className="category-search-ui">
+				<div className="text-wrapper">카테고리 검색하기</div>
+				<div className="search-button">
+					<img
+						className="ellipse"
+						alt="Ellipse"
+						src="ellipse-1.svg"
+					/>
+				</div>
+			</div>
 		</>
 	);
 }
