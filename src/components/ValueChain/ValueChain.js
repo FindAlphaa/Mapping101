d0ee9de03da270beefbd80355afeef562d3feacb
daehyun kim <bigdarkgold@gmail.com>
2023-08-16 19:52:16 +0900

    add valuechain section & footer  in graph page

---
+++ src/components/ValueChain/ValueChain.js
@@ -0,0 +1,11 @@
+import styles from "./ValueChain.module.css";
+
+function ValuechainSection() {
+  return (
+    <div className={styles.valuechainsection}>
+      <div className={styles.valueChainVisual}></div>
+    </div>
+  );
+}
+
+export default ValuechainSection;
--- src/screens/GraphPage.js
+++ src/screens/GraphPage.js
@@ -6,69 +6,73 @@ import NavBar from "../components/NavBar/NavBar";
 import ItemBar from "../components/ItemBar/ItemBar";
 import InfoBar from "../components/InfoBar/InfoBar";
 import Graph from "../components/Graph/Graph";
+import Footer from "../components/Footer/Footer";
+import ValueChain from "../components/ValueChain/ValueChain";
 
 function GraphPage() {
-	const { id } = useParams();
+  const { id } = useParams();
 
-	const [data, setData] = useState();
-	const [loading, setLoading] = useState(true); // Initializing the loading state
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true); // Initializing the loading state
 
-	const itemsMap = {
-		"consumer-goods": {
-			itemName: "경기 관련 소비재",
-			itemDescription: "consumer goods related to the economy",
-		},
-		economy: {
-			itemName: "금융",
-			itemDescription: "Economy",
-		},
-		"industrial-goods": {
-			itemName: "산업재",
-			itemDescription: "Industrial goods",
-		},
-		meterial: {
-			itemName: "소재",
-			itemDescription: "Material",
-		},
-		it: { itemName: "IT", itemDescription: "Internet Technology" },
-		"communication-service": {
-			itemName: "	커뮤니케이션 서비스",
-			itemDescription: "Communication Services",
-		},
-		"essential-goods": {
-			itemName: "	필수 소비재",
-			itemDescription: "Essential Consumer Goods",
-		},
-		energy: {
-			itemName: "에너지",
-			itemDescription: "Energy",
-		},
-		utility: {
-			itemName: "유틸리티",
-			itemDescription: "Utility",
-		},
-		// ... 기타 아이템들
-	};
-	const { itemName, itemDescription } = itemsMap[id] || {};
+  const itemsMap = {
+    "consumer-goods": {
+      itemName: "경기 관련 소비재",
+      itemDescription: "consumer goods related to the economy",
+    },
+    economy: {
+      itemName: "금융",
+      itemDescription: "Economy",
+    },
+    "industrial-goods": {
+      itemName: "산업재",
+      itemDescription: "Industrial goods",
+    },
+    meterial: {
+      itemName: "소재",
+      itemDescription: "Material",
+    },
+    it: { itemName: "IT", itemDescription: "Internet Technology" },
+    "communication-service": {
+      itemName: "	커뮤니케이션 서비스",
+      itemDescription: "Communication Services",
+    },
+    "essential-goods": {
+      itemName: "	필수 소비재",
+      itemDescription: "Essential Consumer Goods",
+    },
+    energy: {
+      itemName: "에너지",
+      itemDescription: "Energy",
+    },
+    utility: {
+      itemName: "유틸리티",
+      itemDescription: "Utility",
+    },
+    // ... 기타 아이템들
+  };
+  const { itemName, itemDescription } = itemsMap[id] || {};
 
-	const fetchData = async () => {
-		const response = await axios.get(`/data/${id}.json`);
-		setData(response.data);
-		setLoading(false); // 데이터 로딩 완료
-	};
-	useEffect(() => {
-		fetchData();
-	}, [data]);
+  const fetchData = async () => {
+    const response = await axios.get(`/data/${id}.json`);
+    setData(response.data);
+    setLoading(false); // 데이터 로딩 완료
+  };
+  useEffect(() => {
+    fetchData();
+  }, [data]);
 
-	return (
-		<>
-			<NavBar />
-			<ItemBar itemName={itemName} itemDescription={itemDescription} />
-			<InfoBar />
+  return (
+    <>
+      <NavBar />
+      <ItemBar itemName={itemName} itemDescription={itemDescription} />
+      <InfoBar />
 
-			{loading ? <div>로딩중..</div> : <Graph data={data} />}
-		</>
-	);
+      {loading ? <div>로딩중..</div> : <Graph data={data} />}
+      <ValueChain />
+      <Footer />
+    </>
+  );
 }
 
 export default GraphPage;
