09adf99764655a1847e61e0701ed65eeb563e79c
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-17 11:20:03 +0900

    react dom 조작 코드 수정

---
+++ src/components/Graph/Link.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Link({ link }) {
+	return (
+		<line
+			x1={link.source.x}
+			y1={link.source.y}
+			x2={link.target.x}
+			y2={link.target.y}
+			stroke="#999"
+			strokeOpacity={0.6}
+		/>
+	);
+}
+
+export default Link;
