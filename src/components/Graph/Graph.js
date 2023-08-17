9855a55e9096b25b5565223ff75770b2ce77e53c
daehyun kim <bigdarkgold@gmail.com>
2023-08-17 15:36:53 +0900

    그래프 노드와 키워드 연결중...

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -4,75 +4,77 @@ import styles from "./Graph.module.css";
 import Node from "./Node";
 import Link from "./Link";
 
-function Graph({ data, width = 2004, height = 1000 }) {
-	const [nodes, setNodes] = useState([...data.nodes]);
-	const [links, setLinks] = useState([...data.links]);
-	const simulationRef = useRef(null);
+function Graph({ data, onNodeClick, width = 2004, height = 1000 }) {
+  const [nodes, setNodes] = useState([...data.nodes]);
+  const [links, setLinks] = useState([...data.links]);
+  const simulationRef = useRef(null);
 
-	useEffect(() => {
-		data.nodes.forEach((node) => {
-			node._children = data.links
-				.filter((link) => link.source === node.id)
-				.map((link) => data.nodes.find((n) => n.id === link.target));
-		});
+  useEffect(() => {
+    data.nodes.forEach((node) => {
+      node._children = data.links
+        .filter((link) => link.source === node.id)
+        .map((link) => data.nodes.find((n) => n.id === link.target));
+    });
 
-		if (simulationRef.current) {
-			simulationRef.current.nodes(nodes);
-		}
+    if (simulationRef.current) {
+      simulationRef.current.nodes(nodes);
+    }
 
-		simulationRef.current = d3
-			.forceSimulation(nodes)
-			.force(
-				"link",
-				d3
-					.forceLink(links)
-					.id((d) => d.id)
-					.distance(100)
-			)
-			.force("charge", d3.forceManyBody().strength(-1000))
-			.force("x", d3.forceX())
-			.force("y", d3.forceY());
+    simulationRef.current = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3
+          .forceLink(links)
+          .id((d) => d.id)
+          .distance(100)
+      )
+      .force("charge", d3.forceManyBody().strength(-1000))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
 
-		simulationRef.current.on("tick", () => {
-			setNodes([...nodes]);
-			setLinks([...links]);
-		});
+    simulationRef.current.on("tick", () => {
+      setNodes([...nodes]);
+      setLinks([...links]);
+    });
 
-		return () => simulationRef.current.stop();
-	}, [nodes]);
+    return () => simulationRef.current.stop();
+  }, [data.links, data.nodes, nodes, links]); // 의존성 배열에 data.links, data.nodes, links 추가
 
-	const handleNodeClick = (node) => {
-		// Add logic here when each node is clicked
-		console.log("Node clicked:", node);
-	};
+  const handleNodeClick = (node) => {
+    // handleNodeClick 클릭한 노드의 id를 상위 컴포넌트로 전달 (GraphPage)
+    // Add logic here when each node is clicked
+    console.log("Node clicked:", node);
+    onNodeClick(node.id); /* 콜백 함수 */
+  };
 
-	return (
-		<div className={styles.graph}>
-			<svg
-				width={width}
-				height={height}
-				viewBox={[-width / 2, -height / 2, width, height]}
-				style={{ maxWidth: "100%", height: "auto" }}
-				// ... other svg properties ...
-			>
-				<g className="links">
-					{links.map((link, index) => (
-						<Link key={index} link={link} />
-					))}
-				</g>
-				<g className="nodes">
-					{nodes.map((node, index) => (
-						<Node
-							key={index}
-							node={node}
-							onClick={handleNodeClick}
-							simulation={simulationRef}
-						/>
-					))}
-				</g>
-			</svg>
-		</div>
-	);
+  return (
+    <div className={styles.graph}>
+      <svg
+        width={width}
+        height={height}
+        viewBox={[-width / 2, -height / 2, width, height]}
+        style={{ maxWidth: "100%", height: "auto" }}
+        // ... other svg properties ...
+      >
+        <g className="links">
+          {links.map((link, index) => (
+            <Link key={index} link={link} />
+          ))}
+        </g>
+        <g className="nodes">
+          {nodes.map((node, index) => (
+            <Node
+              key={index}
+              node={node}
+              onClick={handleNodeClick}
+              simulation={simulationRef}
+            />
+          ))}
+        </g>
+      </svg>
+    </div>
+  );
 }
 
 export default Graph;
--- src/components/ValueChain/ValueChain.js
+++ src/components/ValueChain/ValueChain.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./ValueChain.module.css";
 
-function ValueChain() {
+function ValueChain({ selectedNodeId }) {
+  const [keywordsData, setKeywordsData] = useState(null);
+  //keywordsData, node data, node는 graph.js에서 node.id로 값 받아옴
+
+  useEffect(() => {
+    if (selectedNodeId) {
+      axios
+        .get(`/data/${selectedNodeId}_keyword.json`)
+        .then((keywordResponse) => {
+          console.log("Received keyword data:", keywordResponse.data);
+          console.debug(setKeywordsData(keywordResponse.data));
+          setKeywordsData(keywordResponse.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching keyword data:", error);
+        });
+    }
+  }, [selectedNodeId]);
+
   return (
     <div className={styles.valueChainsection}>
       <div className={styles.valueChainVisual}>
@@ -9,6 +28,18 @@ function ValueChain() {
           <div className={styles.companySection}></div>
         </div>
         <div className={styles.valueChain} />
+        {keywordsData && keywordsData[selectedNodeId] && (
+          <div className={styles.valueChain}>
+            <h3 className={styles.nodeTitle}>{selectedNodeId}</h3>
+            <ul className={styles.keywordList}>
+              {keywordsData[selectedNodeId].map((keyword, kIndex) => (
+                <li key={kIndex} className={styles.keywordItem}>
+                  {keyword}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
