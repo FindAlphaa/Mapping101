86dd03c11256a5a7f50eaa1594840e0d72937534
daehyun kim <bigdarkgold@gmail.com>
2023-08-17 19:44:35 +0900

    company page 추가 (test)

---
+++ src/screens/CompanyPage.js
@@ -0,0 +1,17 @@
+import ItemBar from "../components/ItemBar/ItemBar";
+import Section1 from "../components/Section1/Section1";
+import Section2 from "../components/Section2/Section2";
+import Section3 from "../components/Section3/Section3";
+
+function CompanyPage() {
+  return (
+    <>
+      <ItemBar />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </>
+  );
+}
+
+export default CompanyPage;
