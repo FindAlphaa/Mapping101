2ec04d630b257ad17a30af2a35848773d3fe78e6
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-17 17:53:51 +0900

    정리

--- src/components/Graph/Node.js
+++ src/components/Graph/Node.js
@@ -47,7 +47,7 @@ function Node({ node, onClick, simulation }) {
 			<text
 				className={styles.font}
 				textAnchor="middle"
-				dy={node.radius + 15}
+				dy={node.radius + 20}
 				fontSize={15}
 				fill="white"
 				fontWeight={500}
