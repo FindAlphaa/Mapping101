629388b670267b0ca7784ec04853d63a8038410f
daehyun kim <bigdarkgold@gmail.com>
2023-08-23 17:42:38 +0900

    treemap 시작하기전 배경 흰색 제거

--- client/src/components/RadarGraph/RadarGraph.js
+++ client/src/components/RadarGraph/RadarGraph.js
@@ -8,82 +8,88 @@ import { Chart, RadialLinearScale } from "chart.js";
 Chart.register(RadialLinearScale);
 
 const RadarGraph = ({ selectedNodeId }) => {
-	const [data, setData] = useState(null);
-	const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const PORT = 5000;
-			const response = await axios.get(
-				`http://localhost:${PORT}/api/radar/${selectedNodeId}`
-			);
-			const data = {
-				labels: [
-					"초과이익지속성",
-					"성장성",
-					"수익성",
-					"잠재성",
-					"매출액변동성",
-					"화제성",
-				],
-				datasets: [
-					{
-						label: response.data.label,
-						data: response.data.data,
-						fill: false,
-						backgroundColor: "rgba(255, 99, 132, 0.2)",
-						borderColor: "rgb(255, 99, 132)",
-						pointBackgroundColor: "rgb(255, 99, 132)",
-						pointBorderColor: "#fff",
-						pointHoverBackgroundColor: "#fff",
-						pointHoverBorderColor: "rgb(255, 99, 132)",
-					},
-				],
-			};
-			setData(data);
-			setLoading(false);
-		};
+  useEffect(() => {
+    const fetchData = async () => {
+      const PORT = 4000;
+      let response;
+      try {
+        response = await axios.get(
+          `http://localhost:${PORT}/api/radar/${selectedNodeId}`
+        );
+      } catch (error) {
+        console.log(error);
+        setLoading(true);
+      }
+      const data = {
+        labels: [
+          "초과이익지속성",
+          "성장성",
+          "수익성",
+          "잠재성",
+          "매출액변동성",
+          "화제성",
+        ],
+        datasets: [
+          {
+            label: response.data.label,
+            data: response.data.data,
+            fill: false,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgb(255, 99, 132)",
+            pointBackgroundColor: "rgb(255, 99, 132)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(255, 99, 132)",
+          },
+        ],
+      };
+      setData(data);
+      setLoading(false);
+    };
 
-		if (selectedNodeId) fetchData();
-	}, [selectedNodeId]);
+    if (selectedNodeId) fetchData();
+  }, [selectedNodeId]);
 
-	if (loading || !data) {
-		return <Loading />;
-	}
+  if (loading || !data) {
+    return <Loading />;
+  }
 
-	return (
-		<div className={styles.radarWrapper}>
-			<Radar
-				data={data}
-				className={styles.radarGraph}
-				options={{
-					plugins: {
-						legend: {
-							labels: {
-								color: "white", // 폰트 색상
-								fontSize: 16, // 폰트 크기
-							},
-						},
-					},
-					scales: {
-						r: {
-							angleLines: {
-								color: "gray", // 각 라벨을 이어주는 선의 색상 설정
-							},
-							grid: {
-								color: "gray", // 이 부분을 추가하여 그리드 라인의 색상을 흰색으로 설정
-							},
-							// radial scale
-							pointLabels: {
-								color: "white", // 폰트 색상
-								fontSize: 16, // 폰트 크기
-							},
-						},
-					},
-				}}
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.radarWrapper}>
+      <Radar
+        data={data}
+        className={styles.radarGraph}
+        options={{
+          plugins: {
+            legend: {
+              labels: {
+                color: "white", // 폰트 색상
+                fontSize: 16, // 폰트 크기
+              },
+            },
+          },
+          scales: {
+            r: {
+              angleLines: {
+                color: "gray", // 각 라벨을 이어주는 선의 색상 설정
+              },
+              grid: {
+                color: "gray", // 이 부분을 추가하여 그리드 라인의 색상을 흰색으로 설정
+              },
+              // radial scale
+              pointLabels: {
+                color: "white", // 폰트 색상
+                fontSize: 16, // 폰트 크기
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default RadarGraph;
--- client/src/components/TreeMap/TreeMap.js
+++ client/src/components/TreeMap/TreeMap.js
@@ -3,85 +3,88 @@ import { AgChartsReact } from "ag-charts-react";
 import axios from "axios";
 
 import Loading from "../Loading/Loading";
+import styles from "./TreeMap.module.css";
 import { set } from "mongoose";
 
 function TreeMap({ selectedNodeId }) {
-	const [data, setData] = useState();
+  const [data, setData] = useState();
 
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		setLoading(true);
-		const fetchTreeData = async () => {
-			const PORT = 5000;
-			const res = await axios.get(
-				`http://localhost:${PORT}/api/company/${selectedNodeId}`
-			);
-			const treeData = res.data;
-			setData(treeData);
-			setLoading(false);
-		};
-		if (selectedNodeId) fetchTreeData();
-	}, [selectedNodeId]);
+  useEffect(() => {
+    setLoading(true);
+    const fetchTreeData = async () => {
+      const PORT = 4000;
+      const res = await axios.get(
+        `http://localhost:${PORT}/api/company/${selectedNodeId}`
+      );
+      const treeData = res.data;
+      setData(treeData);
+      setLoading(false);
+    };
+    if (selectedNodeId) fetchTreeData();
+  }, [selectedNodeId]);
 
-	const options = {
-		data,
-		series: [
-			{
-				type: "treemap",
-				labelKey: "name",
-				sizeKey: "size",
-				gradient: false,
-				colorKey: "color",
-				tooltip: {
-					renderer: (params) => {
-						return {
-							content: `ESG: ${params.datum[params.colorKey]}`,
-						};
-					},
-				},
-				formatter: (params) => ({
-					stroke: params.depth < 2 ? "transparent" : "black",
-				}),
-				// labels: {
-				// 	value: {
-				// 		formatter: (params) => `ESG: ${params.datum.color}`,
-				// 	},
-				// },
-			},
-		],
-		background: {
-			fill: "#2c2c2c",
-		},
-		title: {
-			text: selectedNodeId,
-			fontSize: 18,
-			fontWeight: "bold",
-			color: "#fff",
-		},
-		subtitle: {
-			text: "선택된 키워드에 대한 회사 ESG 점수",
-			fontSize: 14,
-			fontWeight: "normal",
-			color: "#fff",
-		},
-	};
-	return (
-		<>
-			{loading ? (
-				<Loading />
-			) : (
-				<div
-					className="ag-theme-alpine"
-					style={{
-						height: "530px",
-					}}
-				>
-					<AgChartsReact options={options} />
-				</div>
-			)}
-		</>
-	);
+  const options = {
+    data,
+    series: [
+      {
+        type: "treemap",
+        labelKey: "name",
+        sizeKey: "size",
+        gradient: false,
+        colorKey: "color",
+        tooltip: {
+          renderer: (params) => {
+            return {
+              content: `ESG: ${params.datum[params.colorKey]}`,
+            };
+          },
+        },
+        formatter: (params) => ({
+          stroke: params.depth < 2 ? "transparent" : "black",
+        }),
+        // labels: {
+        // 	value: {
+        // 		formatter: (params) => `ESG: ${params.datum.color}`,
+        // 	},
+        // },
+      },
+    ],
+    background: {
+      fill: "#2c2c2c",
+    },
+    title: {
+      text: selectedNodeId,
+      fontSize: 18,
+      fontWeight: "bold",
+      color: "#fff",
+    },
+    subtitle: {
+      text: "선택된 키워드에 대한 회사 ESG 점수",
+      fontSize: 14,
+      fontWeight: "normal",
+      color: "#fff",
+    },
+  };
+  return (
+    <div className={styles.treeMapWrapper}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div
+          className="ag-theme-alpine"
+          style={{
+            height: "530px",
+          }}
+        >
+          <div className={styles.treeMapContainer}>
+            <AgChartsReact options={options} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TreeMap;
