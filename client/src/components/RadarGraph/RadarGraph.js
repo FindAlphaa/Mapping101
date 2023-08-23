c89ffc4a7d46891cec8dcaa4f95c3f43d11e7f03
daehyun kim <bigdarkgold@gmail.com>
2023-08-23 19:02:11 +0900

    오류 렌더링 로직 추가 & 노드 문제 해결
    데이터 없는 노드 눌렀고, 데이터 있는 노드 누르면 그래프가 안나오는 문제 해결

--- client/src/components/RadarGraph/RadarGraph.js
+++ client/src/components/RadarGraph/RadarGraph.js
@@ -10,57 +10,80 @@ Chart.register(RadialLinearScale);
 const RadarGraph = ({ selectedNodeId }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); // error handling
 
   useEffect(() => {
+    // 초기 상태로 데이터와 오류 상태를 초기화
+    setData(null);
+    setError(null);
+
     const fetchData = async () => {
       const PORT = 4000;
-      let response;
       try {
-        response = await axios.get(
+        const response = await axios.get(
           `http://localhost:${PORT}/api/radar/${selectedNodeId}`
         );
+
+        // 응답이 정상적으로 왔고 데이터가 있는 경우
+        if (response && response.data) {
+          // 응답받은 데이터를 사용하여 차트 데이터 구성
+          const data = {
+            labels: [
+              "초과이익지속성",
+              "성장성",
+              "수익성",
+              "잠재성",
+              "매출액변동성",
+              "화제성",
+            ],
+            datasets: [
+              {
+                label: response.data.label,
+                data: response.data.data,
+                fill: false,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgb(255, 99, 132)",
+                pointBackgroundColor: "rgb(255, 99, 132)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgb(255, 99, 132)",
+              },
+            ],
+          };
+          setData(data); // 차트 데이터 상태 업데이트
+          setLoading(false); // 로딩 상태 해제
+        } else {
+          // 응답에 데이터가 없는 경우
+          setData(null);
+          setLoading(false);
+        }
       } catch (error) {
+        // API 요청 중 오류 발생 시 처리
         console.log(error);
-        setLoading(true);
+        setError(error.message); // 오류 메시지 상태 업데이트
+        setLoading(false); // 로딩 상태 해제
+        setData(null); // 데이터 상태 초기화
       }
-      const data = {
-        labels: [
-          "초과이익지속성",
-          "성장성",
-          "수익성",
-          "잠재성",
-          "매출액변동성",
-          "화제성",
-        ],
-        datasets: [
-          {
-            label: response.data.label,
-            data: response.data.data,
-            fill: false,
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgb(255, 99, 132)",
-            pointBackgroundColor: "rgb(255, 99, 132)",
-            pointBorderColor: "#fff",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgb(255, 99, 132)",
-          },
-        ],
-      };
-      setData(data);
-      setLoading(false);
     };
 
+    // 선택된 노드 ID가 있으면 데이터 요청
     if (selectedNodeId) fetchData();
   }, [selectedNodeId]);
 
+  // 로딩 중이거나 데이터가 없을 경우 로딩 컴포넌트 렌더링
   if (loading || !data) {
+    return <Loading />;
+  }
+
+  // 오류 발생 시 로딩 컴포넌트 렌더링 (오류 메시지 표시를 위해 수정할 수 있습니다.)
+  if (error) {
     return <Loading />;
   }
 
   return (
     <div className={styles.radarWrapper}>
       <Radar
-        data={data}
+        data={loading ? null : data}
         className={styles.radarGraph}
         options={{
           plugins: {
